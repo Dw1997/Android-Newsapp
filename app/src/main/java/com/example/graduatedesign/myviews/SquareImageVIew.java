859a32f6abcb3e6de958c07b863c48b81a946ff7
Dw1997 <dwsdsghh@163.com>
2019-11-03 14:22:23 +0800

    '2019-11-3-update'

---
+++ app/src/main/java/com/example/graduatedesign/myviews/SquareImageVIew.java
@@ -0,0 +1,52 @@
+package com.example.graduatedesign.myviews;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+@SuppressLint("AppCompatCustomView")
+public class SquareImageVIew extends ImageView {
+    public SquareImageVIew(Context context) {
+        super(context);
+    }
+
+    public SquareImageVIew(Context context, AttributeSet attrs) {
+
+        this(context, attrs, 0);
+
+    }
+
+
+
+    public SquareImageVIew(Context context, AttributeSet attrs, int defStyle) {
+
+        super(context, attrs, defStyle);
+
+
+
+    }
+
+
+
+    @Override
+
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+        setMeasuredDimension(getDefaultSize(0, widthMeasureSpec), getDefaultSize(0, heightMeasureSpec));
+
+
+
+        int childWidthSize = getMeasuredWidth();
+
+        //高度和宽度一样
+
+        heightMeasureSpec = widthMeasureSpec = MeasureSpec.makeMeasureSpec(childWidthSize, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+    }
+}
+
+
+
